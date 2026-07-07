43f87e05ecc1e31687a3b156bbb7900cd3ea5f60
NUR NAJWA SYAFIQAH ABU BAKAR <2024776181@student.uitm.edu.my>
2026-07-07 18:52:19 +0800

    Add ProfileActivity and logout functionality

--- app/src/main/java/com/example/kucingputeh/ProfileActivity.java
+++ app/src/main/java/com/example/kucingputeh/ProfileActivity.java
@@ -6,19 +6,27 @@ import android.widget.Button;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import com.example.kucingputeh.remote.LoginActivity;
+import com.example.kucingputeh.remote.SharedPrefManager;
+
 public class ProfileActivity extends AppCompatActivity {
 
     Button btnLogout;
+    SharedPrefManager spm;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_profile);
 
+        spm = new SharedPrefManager(getApplicationContext());
+
         btnLogout = findViewById(R.id.btnLogout);
 
         btnLogout.setOnClickListener(v -> {
-            Intent intent = new Intent(ProfileActivity.this, MainActivity.class);
+            spm.logout();
+
+            Intent intent = new Intent(ProfileActivity.this, LoginActivity.class);
             intent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK | Intent.FLAG_ACTIVITY_CLEAR_TASK);
             startActivity(intent);
             finish();
