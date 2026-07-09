bce33608c56f31bf7cabb51435e8ed3642fdee60
AfiqahIsmail <2024553775@student.uitm.edu.my>
2026-07-09 14:10:16 +0800

    profile

--- app/src/main/java/com/example/kucingputeh/MainActivity.java
+++ app/src/main/java/com/example/kucingputeh/MainActivity.java
@@ -3,7 +3,6 @@ package com.example.kucingputeh;
 import android.annotation.SuppressLint;
 import android.content.Intent;
 import android.os.Bundle;
-import android.view.View;
 import android.widget.Button;
 import android.widget.Toast;
 
@@ -50,7 +49,7 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
 
         // 1. TUKAR DI SINI kepada layout file 'Home' anda
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_homepage);
 
         spm = new SharedPrefManager(getApplicationContext());
 
