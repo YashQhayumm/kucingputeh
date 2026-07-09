4f3c2d67ff07a8a68ebd83c47eafff4e872727e2
NUR NAJWA SYAFIQAH ABU BAKAR <2024776181@student.uitm.edu.my>
2026-07-09 15:20:10 +0800

    Add Update Profile button functionality

--- app/src/main/java/com/example/kucingputeh/ProfileActivity.java
+++ app/src/main/java/com/example/kucingputeh/ProfileActivity.java
@@ -3,6 +3,8 @@ package com.example.kucingputeh;
 import android.content.Intent;
 import android.os.Bundle;
 import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
 
 import androidx.appcompat.app.AppCompatActivity;
 
@@ -11,7 +13,8 @@ import com.example.kucingputeh.remote.SharedPrefManager;
 
 public class ProfileActivity extends AppCompatActivity {
 
-    Button btnLogout;
+    EditText etName, etStudentID, etEmail, etPassword;
+    Button btnUpdate, btnLogout;
     SharedPrefManager spm;
 
     @Override
@@ -21,7 +24,24 @@ public class ProfileActivity extends AppCompatActivity {
 
         spm = new SharedPrefManager(getApplicationContext());
 
+        etName = findViewById(R.id.etName);
+        etStudentID = findViewById(R.id.etStudentID);
+        etEmail = findViewById(R.id.etEmail);
+        etPassword = findViewById(R.id.etPassword);
+
+        btnUpdate = findViewById(R.id.btnUpdate);
         btnLogout = findViewById(R.id.btnLogout);
+
+        btnUpdate.setOnClickListener(v -> {
+            String name = etName.getText().toString().trim();
+            String studentID = etStudentID.getText().toString().trim();
+            String email = etEmail.getText().toString().trim();
+            String password = etPassword.getText().toString().trim();
+
+            Toast.makeText(ProfileActivity.this,
+                    "Update Profile clicked",
+                    Toast.LENGTH_SHORT).show();
+        });
 
         btnLogout.setOnClickListener(v -> {
             spm.logout();
